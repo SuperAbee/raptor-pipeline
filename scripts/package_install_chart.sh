8f3b161e1f9c1b5410c192681c2b8474964a645e
clareyao <clareyao@tencent.com>
2022-03-16 19:02:24 +0800

    fix

--- scripts/package_install_chart.sh
+++ scripts/package_install_chart.sh
@@ -2,6 +2,11 @@
 
 rm raptor-chart -rf
 git clone https://github.com/SuperAbee/raptor-chart.git
-helm package raptor-chart/
+
+helm package raptor-chart/raptor/
 helm delete raptor -n default
 helm install raptor raptor-0.1.0.tgz -n default
+
+helm package raptor-chart/raptor-hello/
+helm delete raptor-hello -n default
+helm install raptor-hello raptor-hello-0.1.0.tgz -n default
